955b0c76685c0b56f7cb25ada968b93c05bcb2bd
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-08-21 00:20:45 +0100

    updates

--- components/Hero.js
+++ components/Hero.js
@@ -1,18 +1,21 @@
-import Link from 'next/link'
+import Link from "next/link";
 
-import styles from './Hero.module.scss';
+import styles from "./Hero.module.scss";
 
 const Hero = () => {
-    return (
-        <div className={styles.hero}>
-            <div className={styles.hero__info}>
-                <h1 className={styles.hero__title}>Fashionbulous</h1>
-                <p className={styles.hero__tagline}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus.</p>
-                <a className={styles.hero__button} href='/shop'>Shop</a>
-            </div>
-            
-        </div>
-    )
-}
+  return (
+    <div className={styles.hero}>
+      <div className={styles.hero__info}>
+        <h1 className={styles.hero__title}>Fashionbulous</h1>
+        <p className={styles.hero__tagline}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus.
+        </p>
+        <a className={styles.hero__button} href="/shop">
+          Shop
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
--- components/Product.js
+++ components/Product.js
@@ -1,17 +1,33 @@
-import Image from 'next/image';
-import styles from './Product.module.scss'
+import Image from "next/image";
+import styles from "./Product.module.scss";
 
-const Product = ({ productName, productImg, productPrice, isRect, product__desc }) => {
-  const rectClassName = styles.rectProduct
+const Product = ({
+  productName,
+  productImg,
+  productPrice,
+  isRect,
+  product__desc,
+}) => {
+  const rect = {
+    product: styles.rectProduct,
+    product__name: styles.rectProduct__name,
+    product__price: styles.rectProduct__price,
+  };
 
-    return (
-      <div className={isRect ? reactClassName : styles.Product}>
-        <Image width={250} height={300} object-fit='contain' src={productImg} alt={productName} className="product__image" />
+  return (
+    <div className={isRect ? rect.product : styles.Product}>
+      <img
+        src={productImg}
+        alt={productName}
+        className={styles.product__image}
+      />
+      <div className={styles.product__info}>
         <h3 className="product__name">{productName}</h3>
-        { isRect ? <p className="product__desc">{product__desc}</p> : null }
+        {isRect ? <p className="product__desc">{product__desc}</p> : null}
         <small className="product__price">{productPrice}</small>
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default Product
+export default Product;
